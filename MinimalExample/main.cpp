b9506ef5546f185a13a338086d2b7cf0c12eb42d
Todd <1912229135@qq.com>
2026-03-28 15:05:52 +0800

    Signed-off-by: Todd <1912229135@qq.com>

--- MinimalExample/main.cpp
+++ MinimalExample/main.cpp
@@ -4,7 +4,27 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_LINUX
+    qputenv("QT_QPA_PLATFORM", "xcb");
+#endif
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
+        Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+    QCoreApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
+
     QApplication a(argc, argv);
+    a.setStyle("Fusion");
+
+    QFont font;
+    font.setFamilies({"Microsoft YaHei", "PingFang SC", "Segoe UI"});
+    font.setPixelSize(14);
+    a.setFont(font);
+
     Widget w;
     w.show();
     return QCoreApplication::exec();
