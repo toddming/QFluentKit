56ded722e6d502a0b6bb9d212262b833107323e2
Todd <1912229135@qq.com>
2026-04-22 14:45:15 +0800

    优化: 重构 SpinBox
    Signed-off-by: Todd <1912229135@qq.com>

--- QFluent/src/QFluent/SpinBox.cpp
+++ QFluent/src/QFluent/SpinBox.cpp
@@ -1,19 +1,18 @@
-﻿#include "SpinBox.h"
+#include "SpinBox.h"
+
 #include <QMenu>
 #include <QLineEdit>
-#include <QPainterPath>
 #include <QHBoxLayout>
-#include <QColor>
+#include <QPainter>
+#include <QPainterPath>
 #include <QStyleOption>
 #include <QContextMenuEvent>
 #include <QMouseEvent>
 #include <QApplication>
-#include <QPainter>
 
 #include "Theme.h"
 #include "FluentIcon.h"
 #include "StyleSheet.h"
-
 
 template class InlineSpinBoxBase<QSpinBox>;
 template class InlineSpinBoxBase<QDoubleSpinBox>;
@@ -21,30 +20,36 @@ template class InlineSpinBoxBase<QTimeEdit>;
 template class InlineSpinBoxBase<QDateTimeEdit>;
 template class InlineSpinBoxBase<QDateEdit>;
 
-
 // --- SpinButton ---
 
-SpinButton::SpinButton(const FluentIconBase &icon, QWidget* parent)
-    : QToolButton(parent), m_fluentIcon(icon.clone()) {
+SpinButton::SpinButton(const FluentIconBase &icon, QWidget *parent)
+    : QToolButton(parent)
+    , m_fluentIcon(icon.clone())
+{
     setFixedSize(sizeHint());
     setCursor(Qt::ArrowCursor);
-
-    setFixedSize(31, 23);
     setIconSize(QSize(10, 10));
-    StyleSheet::registerWidget(this, Fluent::ThemeStyle::SPIN_BOX);
 }
 
-void SpinButton::mousePressEvent(QMouseEvent* e) {
+QSize SpinButton::sizeHint() const
+{
+    return QSize(31, 23);
+}
+
+void SpinButton::mousePressEvent(QMouseEvent *e)
+{
     m_isPressed = true;
     QToolButton::mousePressEvent(e);
 }
 
-void SpinButton::mouseReleaseEvent(QMouseEvent* e) {
+void SpinButton::mouseReleaseEvent(QMouseEvent *e)
+{
     m_isPressed = false;
     QToolButton::mouseReleaseEvent(e);
 }
 
-void SpinButton::paintEvent(QPaintEvent* e) {
+void SpinButton::paintEvent(QPaintEvent *e)
+{
     QToolButton::paintEvent(e);
 
     QPainter painter(this);
@@ -59,37 +64,45 @@ void SpinButton::paintEvent(QPaintEvent* e) {
     FluentIconUtils::drawIcon(*m_fluentIcon, &painter, QRectF(10, 6.5, 11, 11));
 }
 
-SpinBoxBase::SpinBoxBase(QWidget* host)
+// --- SpinBoxBase ---
+
+SpinBoxBase::SpinBoxBase(QWidget *host)
     : m_parentWidget(host)
 {
-
 }
 
 SpinBoxBase::~SpinBoxBase() = default;
 
-void SpinBoxBase::setup() {
+void SpinBoxBase::setup()
+{
     setupStyle();
     connectButtons();
 }
 
-void SpinBoxBase::setupStyle() {
+void SpinBoxBase::setupStyle()
+{
+    if (!m_parentWidget)
+        return;
+
     m_parentWidget->setProperty("transparent", true);
     StyleSheet::registerWidget(m_parentWidget, Fluent::ThemeStyle::SPIN_BOX);
 
-    if (auto *spin = qobject_cast<QAbstractSpinBox*>(m_parentWidget)) {
+    if (auto *spin = qobject_cast<QAbstractSpinBox *>(m_parentWidget))
         spin->setButtonSymbols(QAbstractSpinBox::NoButtons);
-    }
 
     m_parentWidget->setFixedHeight(33);
     m_parentWidget->setAttribute(Qt::WA_MacShowFocusRect, false);
     m_parentWidget->setContextMenuPolicy(Qt::NoContextMenu);
 }
 
-void SpinBoxBase::addUpDownButtons() {
-    if (m_layout || !m_parentWidget) return;
+void SpinBoxBase::addUpDownButtons()
+{
+    if (m_layout || !m_parentWidget)
+        return;
 
-    auto* le = lineEdit();
-    if (!le) return;
+    auto *le = lineEdit();
+    if (!le)
+        return;
 
     m_layout = new QHBoxLayout(m_parentWidget);
     m_layout->setContentsMargins(10, 4, 4, 4);
@@ -106,27 +119,19 @@ void SpinBoxBase::addUpDownButtons() {
     m_parentWidget->setMinimumWidth(60);
 }
 
-void SpinBoxBase::connectButtons() {
-    if (auto* spin = qobject_cast<QSpinBox*>(m_parentWidget)) {
-        QObject::connect(m_upButton, &QToolButton::clicked, spin, &QSpinBox::stepUp);
-        QObject::connect(m_downButton, &QToolButton::clicked, spin, &QSpinBox::stepDown);
-    } else if (auto* spin = qobject_cast<QDoubleSpinBox*>(m_parentWidget)) {
-        QObject::connect(m_upButton, &QToolButton::clicked, spin, &QDoubleSpinBox::stepUp);
-        QObject::connect(m_downButton, &QToolButton::clicked, spin, &QDoubleSpinBox::stepDown);
-    } else if (auto* te = qobject_cast<QTimeEdit*>(m_parentWidget)) {
-        QObject::connect(m_upButton, &QToolButton::clicked, te, &QTimeEdit::stepUp);
-        QObject::connect(m_downButton, &QToolButton::clicked, te, &QTimeEdit::stepDown);
-    } else if (auto* dte = qobject_cast<QDateTimeEdit*>(m_parentWidget)) {
-        QObject::connect(m_upButton, &QToolButton::clicked, dte, &QDateTimeEdit::stepUp);
-        QObject::connect(m_downButton, &QToolButton::clicked, dte, &QDateTimeEdit::stepDown);
-    } else if (auto* de = qobject_cast<QDateEdit*>(m_parentWidget)) {
-        QObject::connect(m_upButton, &QToolButton::clicked, de, &QDateEdit::stepUp);
-        QObject::connect(m_downButton, &QToolButton::clicked, de, &QDateEdit::stepDown);
+void SpinBoxBase::connectButtons()
+{
+    if (auto *spin = qobject_cast<QAbstractSpinBox *>(m_parentWidget)) {
+        QObject::connect(m_upButton, &QToolButton::clicked, spin, &QAbstractSpinBox::stepUp);
+        QObject::connect(m_downButton, &QToolButton::clicked, spin, &QAbstractSpinBox::stepDown);
     }
 }
 
-void SpinBoxBase::setError(bool isError) {
-    if (m_isError == isError) return;
+void SpinBoxBase::setError(bool isError)
+{
+    if (!m_parentWidget || m_isError == isError)
+        return;
+
     m_isError = isError;
     m_parentWidget->setProperty("isError", isError);
     m_parentWidget->style()->unpolish(m_parentWidget);
@@ -134,33 +139,85 @@ void SpinBoxBase::setError(bool isError) {
     m_parentWidget->update();
 }
 
-void SpinBoxBase::setSymbolVisible(bool visible) {
-    m_symbolVisible = visible;
-    if (m_upButton) m_upButton->setVisible(visible);
-    if (m_downButton) m_downButton->setVisible(visible);
+bool SpinBoxBase::isError() const
+{
+    return m_isError;
 }
 
-QWidget* SpinBoxBase::lineEdit() const {
-    if (auto* spin = qobject_cast<QAbstractSpinBox*>(m_parentWidget)) {
-        return spin->findChild<QLineEdit*>();
-    }
+void SpinBoxBase::setSymbolVisible(bool visible)
+{
+    m_symbolVisible = visible;
+    if (!m_parentWidget)
+        return;
+
+    if (m_upButton)
+        m_upButton->setVisible(visible);
+    if (m_downButton)
+        m_downButton->setVisible(visible);
+}
+
+QWidget *SpinBoxBase::lineEdit() const
+{
+    if (auto *spin = qobject_cast<QAbstractSpinBox *>(m_parentWidget))
+        return spin->findChild<QLineEdit *>();
     return nullptr;
 }
 
-void SpinBoxBase::drawFocusBorder(QPainter* painter, const QRect& rect) {
+void SpinBoxBase::drawFocusBorder(QPainter *painter, const QRect &rect)
+{
+    if (!painter)
+        return;
+
     painter->setRenderHint(QPainter::Antialiasing);
     painter->setPen(Qt::NoPen);
 
     QPainterPath path;
     int w = rect.width();
     int h = rect.height();
-    path.addRoundedRect(QRectF(0, h-10, w, 10), 5, 5);
+    path.addRoundedRect(QRectF(0, h - 10, w, 10), 5, 5);
 
     QPainterPath rectPath;
-    rectPath.addRect(0, h-10, w, 8);
+    rectPath.addRect(0, h - 10, w, 8);
     path = path.subtracted(rectPath);
+
     painter->fillPath(path, Theme::themeColor(Fluent::ThemeColor::PRIMARY));
 }
 
+// --- InlineSpinBoxBase ---
 
+template <typename T>
+InlineSpinBoxBase<T>::InlineSpinBoxBase(QWidget *parent)
+    : T(parent)
+    , m_helper(new SpinBoxBase(this))
+{
+    m_helper->addUpDownButtons();
+    m_helper->setup();
+}
 
+template <typename T>
+void InlineSpinBoxBase<T>::setError(bool isError)
+{
+    m_helper->setError(isError);
+}
+
+template <typename T>
+bool InlineSpinBoxBase<T>::isError() const
+{
+    return m_helper->isError();
+}
+
+template <typename T>
+void InlineSpinBoxBase<T>::setSymbolVisible(bool visible)
+{
+    m_helper->setSymbolVisible(visible);
+}
+
+template <typename T>
+void InlineSpinBoxBase<T>::paintEvent(QPaintEvent *event)
+{
+    T::paintEvent(event);
+    if (this->hasFocus() && !isError()) {
+        QPainter painter(this);
+        m_helper->drawFocusBorder(&painter, this->rect());
+    }
+}
--- QFluent/src/QFluent/SpinBox.h
+++ QFluent/src/QFluent/SpinBox.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <QSpinBox>
 #include <QDoubleSpinBox>
@@ -7,58 +7,54 @@
 #include <QDateTimeEdit>
 #include <QToolButton>
 #include <QPointer>
-#include <QPainter>
+#include <QScopedPointer>
 
 #include "FluentGlobal.h"
 #include "FluentIcon.h"
 
 class QHBoxLayout;
-class SpinButton : public QToolButton {
+class QPainter;
+class QRect;
+
+class SpinButton : public QToolButton
+{
     Q_OBJECT
 
 public:
-    explicit SpinButton(const FluentIconBase &icon, QWidget* parent = nullptr);
-
-    QSize sizeHint() const override { return QSize(31, 23); }
+    explicit SpinButton(const FluentIconBase &icon, QWidget *parent = nullptr);
+    QSize sizeHint() const override;
 
 protected:
-    void mousePressEvent(QMouseEvent* e) override;
-    void mouseReleaseEvent(QMouseEvent* e) override;
-    void paintEvent(QPaintEvent* e) override;
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+    void paintEvent(QPaintEvent *e) override;
 
 private:
     std::unique_ptr<FluentIconBase> m_fluentIcon;
     bool m_isPressed = false;
 };
 
-// ========================
-// SpinBoxBase 辅助类
-// ========================
-class QFLUENT_EXPORT SpinBoxBase {
+class QFLUENT_EXPORT SpinBoxBase
+{
     Q_DISABLE_COPY(SpinBoxBase)
+
 public:
-    explicit SpinBoxBase(QWidget* host);
+    explicit SpinBoxBase(QWidget *host);
     ~SpinBoxBase();
 
     void setup();
     void addUpDownButtons();
     void setError(bool isError);
-    bool isError() const { return m_isError; }
+    bool isError() const;
     void setSymbolVisible(bool visible);
-    void drawFocusBorder(QPainter* painter, const QRect& rect);
-    QWidget* lineEdit() const;
-
-    // 转发函数（不再是纯虚）
-    bool hasFocus() const { return m_parentWidget ? m_parentWidget->hasFocus() : false; }
-    bool isEnabled() const { return m_parentWidget ? m_parentWidget->isEnabled() : false; }
-    void update() { if (m_parentWidget) m_parentWidget->update(); }
-    void setProperty(const char* name, bool value) { if (m_parentWidget) m_parentWidget->setProperty(name, value); }
+    void drawFocusBorder(QPainter *painter, const QRect &rect);
+    QWidget *lineEdit() const;
 
 private:
     QPointer<QWidget> m_parentWidget;
-    QHBoxLayout* m_layout = nullptr;
-    SpinButton* m_upButton = nullptr;
-    SpinButton* m_downButton = nullptr;
+    QHBoxLayout *m_layout = nullptr;
+    SpinButton *m_upButton = nullptr;
+    SpinButton *m_downButton = nullptr;
     bool m_isError = false;
     bool m_symbolVisible = true;
 
@@ -66,57 +62,67 @@ private:
     void connectButtons();
 };
 
-// ========================
-// 模板 Mixin：InlineSpinBoxBase
-// ========================
 template <typename T>
-class QFLUENT_EXPORT InlineSpinBoxBase : public T {
-    static_assert(
-        std::is_base_of_v<QAbstractSpinBox, T> ||
-            std::is_base_of_v<QDateTimeEdit, T>,
-        "T must derive from QAbstractSpinBox or QDateTimeEdit"
-        );
-
+class QFLUENT_EXPORT InlineSpinBoxBase : public T
+{
+    static_assert(std::is_base_of_v<QAbstractSpinBox, T>,
+                  "T must derive from QAbstractSpinBox");
 
 public:
-    explicit InlineSpinBoxBase(QWidget* parent = nullptr)
-        : T(parent), m_helper(new SpinBoxBase(this))
-    {
-        m_helper->addUpDownButtons();
+    explicit InlineSpinBoxBase(QWidget *parent = nullptr);
 
-        m_helper->setup();
-    }
-
-    void setError(bool isError) {
-        m_helper->setError(isError);
-    }
-
-    bool isError() const {
-        return m_helper->isError();
-    }
-
-    void setSymbolVisible(bool visible) {
-        m_helper->setSymbolVisible(visible);
-    }
+    void setError(bool isError);
+    bool isError() const;
+    void setSymbolVisible(bool visible);
 
 protected:
-    void paintEvent(QPaintEvent* event) override {
-        T::paintEvent(event);
-        if (this->hasFocus() && !isError()) {
-            QPainter painter(this);
-            m_helper->drawFocusBorder(&painter, this->rect());
-        }
-    }
+    void paintEvent(QPaintEvent *event) override;
 
 private:
     QScopedPointer<SpinBoxBase> m_helper;
 };
 
-// ========================
-// 具体控件类型（使用模板实例化）
-// ========================
-class QFLUENT_EXPORT SpinBox        : public InlineSpinBoxBase<QSpinBox>         { Q_OBJECT public: using Base = InlineSpinBoxBase<QSpinBox>; using Base::Base; };
-class QFLUENT_EXPORT DoubleSpinBox  : public InlineSpinBoxBase<QDoubleSpinBox>   { Q_OBJECT public: using Base = InlineSpinBoxBase<QDoubleSpinBox>; using Base::Base; };
-class QFLUENT_EXPORT TimeEdit       : public InlineSpinBoxBase<QTimeEdit>        { Q_OBJECT public: using Base = InlineSpinBoxBase<QTimeEdit>; using Base::Base; };
-class QFLUENT_EXPORT DateTimeEdit   : public InlineSpinBoxBase<QDateTimeEdit>    { Q_OBJECT public: using Base = InlineSpinBoxBase<QDateTimeEdit>; using Base::Base; };
-class QFLUENT_EXPORT DateEdit       : public InlineSpinBoxBase<QDateEdit>        { Q_OBJECT public: using Base = InlineSpinBoxBase<QDateEdit>; using Base::Base; };
+class QFLUENT_EXPORT SpinBox : public InlineSpinBoxBase<QSpinBox>
+{
+    Q_OBJECT
+
+public:
+    using Base = InlineSpinBoxBase<QSpinBox>;
+    using Base::Base;
+};
+
+class QFLUENT_EXPORT DoubleSpinBox : public InlineSpinBoxBase<QDoubleSpinBox>
+{
+    Q_OBJECT
+
+public:
+    using Base = InlineSpinBoxBase<QDoubleSpinBox>;
+    using Base::Base;
+};
+
+class QFLUENT_EXPORT TimeEdit : public InlineSpinBoxBase<QTimeEdit>
+{
+    Q_OBJECT
+
+public:
+    using Base = InlineSpinBoxBase<QTimeEdit>;
+    using Base::Base;
+};
+
+class QFLUENT_EXPORT DateTimeEdit : public InlineSpinBoxBase<QDateTimeEdit>
+{
+    Q_OBJECT
+
+public:
+    using Base = InlineSpinBoxBase<QDateTimeEdit>;
+    using Base::Base;
+};
+
+class QFLUENT_EXPORT DateEdit : public InlineSpinBoxBase<QDateEdit>
+{
+    Q_OBJECT
+
+public:
+    using Base = InlineSpinBoxBase<QDateEdit>;
+    using Base::Base;
+};
